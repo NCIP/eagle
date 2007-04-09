877f120209ea390ed640de585349e32272b5e216
Ryan Landy <landyr>
2007-04-09 18:34:03 +0000

    initial checkin, stubs
    SVN-Revision: 5795

---
+++ src/gov/nih/nci/eagle/web/struts/ClassComparisonAction.java
@@ -0,0 +1,38 @@
+package gov.nih.nci.eagle.web.struts;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+/**
+ * all instance variables (objects) in this action are injected
+ * by the Spring container (application-context-services.xml). The
+ * struts action itself is managed by spring by use of the
+ * org.springframework.web.struts.DelegatingActionProxy class. The
+ * action path can then be referenced by Spring in application-context-struts.xml)
+ * All struts and spring config files can be found in the WEB-INF directory.
+ * @author landyr
+ *
+ */
+
+public class ClassComparisonAction extends DispatchAction{
+    public ActionForward submit(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws IOException	{
+               
+            return (mapping.findForward("success"));
+    }
+    
+    public ActionForward setup(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)throws Exception {
+        
+        return mapping.findForward("failure");
+    } 
+    
+}
