95a2b230f54bb802bd4ae26de6d67934d3faaf26
Ryan Landy <landyr>
2007-04-10 21:44:05 +0000

    load the lists in setup method
    SVN-Revision: 5825

--- src/gov/nih/nci/eagle/web/struts/ClassComparisonAction.java
+++ src/gov/nih/nci/eagle/web/struts/ClassComparisonAction.java
@@ -1,6 +1,13 @@
 package gov.nih.nci.eagle.web.struts;
 
+import gov.nih.nci.caintegrator.application.lists.ListType;
+import gov.nih.nci.caintegrator.application.lists.UserList;
+import gov.nih.nci.caintegrator.application.lists.UserListBeanHelper;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +17,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.util.LabelValueBean;
 
 /**
  * all instance variables (objects) in this action are injected
@@ -31,6 +39,14 @@ public class ClassComparisonAction extends DispatchAction{
     
     public ActionForward setup(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)throws Exception {
+        
+    	UserListBeanHelper helper = new UserListBeanHelper(request.getSession());
+        List<UserList> patientLists = helper.getLists(ListType.PatientDID);
+        List<LabelValueBean> sampleGroups = new ArrayList<LabelValueBean>();
+        for(UserList patientList: patientLists){
+        	sampleGroups.add(new LabelValueBean(patientList.getName(),patientList.getClass().getCanonicalName() + "#" + patientList.getName()));
+        }
+        ((ClassComparisonForm) form).setExistingGroups(sampleGroups);
         
         return mapping.findForward("success");
     } 
