d3dae90121372999e6ba19c53d98e4b37a477c27
ram_bhattaru <ram_bhattaru>
2007-04-23 18:34:37 +0000

    testing
    SVN-Revision: 5894

--- src/gov/nih/nci/eagle/util/EAGLEConstants.java
+++ src/gov/nih/nci/eagle/util/EAGLEConstants.java
@@ -1,16 +1,16 @@
 package gov.nih.nci.eagle.util;
 
 public final class EAGLEConstants {
-    
+
     public static final String userInfoBean = "userInfoBean";
     public static final String loggedIn = "loggedIn";
     public static final String NOT_INCLUDED = "none";
     public static final String ALL = "all";
     public static final int PB_BLAST_COLUMNS = 2;
     public static final int BM_BLAST_COLUMNS = 4;
-    public static final String  APPLICATION_RESOURCES = "ApplicationResources";
-    
-    
+    public static final String  APPLICATION_RESOURCES = "ApplicationResources"; //test
+
+
     public static String getCSMAppName()	{
 		String appname = System.getProperty("gov.nih.nci.eagle.csm_app_name") != null ? (String) System.getProperty("gov.nih.nci.eagle.csm_app_name") : "eagle";
 		return appname;
