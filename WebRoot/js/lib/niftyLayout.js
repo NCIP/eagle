c30cef74bef57697d8f1418164c178acb25393cc
Ryan Landy <landyr>
2007-04-24 13:30:20 +0000

    fix IE 6 nifty arguments JS error
    SVN-Revision: 5902

--- WebRoot/js/lib/niftyLayout.js
+++ WebRoot/js/lib/niftyLayout.js
@@ -1,11 +1,20 @@
 /*nifty corners layout*/
 
-window.onload=function(){
-Nifty("div#menu a","small transparent top");
-Nifty("ul#intro li","same-height");
-Nifty("div.date");
-Nifty("div#content,div#side","same-height");
-Nifty("div.comments div");
-Nifty("div#footer");
-Nifty("div#container","bottom");
+NiftyLoad=function(){
+try	{
+	Nifty("div#menu a","small transparent top");
+	Nifty("ul#intro li","same-height");
+	
+	Nifty("div.date");
+
+//	Nifty("div#content,div#side","same-height");
+	Nifty("div#content", "same-height");
+	Nifty("div#side", "same-height");
+	Nifty("div.comments div");
+	Nifty("div#footer");
+	Nifty("div#container","bottom");
+	}
+catch(er)	{
+//	debug(er.description);
+}
 }
