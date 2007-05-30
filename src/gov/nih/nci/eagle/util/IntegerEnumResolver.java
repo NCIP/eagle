98e3921280a78554bf1d414c4aa99fc57964d2b1
Andrew Shinohara <Andrew.Shinohara@fda.hhs.gov>
2007-05-30 18:48:33 +0000

    utility class to look up enum values
    SVN-Revision: 6105

---
+++ src/gov/nih/nci/eagle/util/IntegerEnumResolver.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.eagle.util;
+
+import gov.nih.nci.caintegrator.studyQueryService.dto.epi.IntegerValueEnum;
+
+public class IntegerEnumResolver {
+
+    public static <T> T resolveEnum(Class<T> enumType, String val) {
+        T[] enumConstants = enumType.getEnumConstants();
+        for(int i = 0; i < enumConstants.length; i++) {
+            if(enumConstants[i] instanceof IntegerValueEnum) {
+                int value = Integer.parseInt(val);
+                if(((IntegerValueEnum)enumConstants[i]).getValue() == value)
+                    return enumConstants[i];
+            }
+        }
+        return null;
+    }
+}
