b36626aec093b636d4e09ccda11953c52e0575b8
Ryan Landy <landyr>
2007-08-30 21:53:02 +0000

    added JSON methods
    SVN-Revision: 6281

--- src/gov/nih/nci/eagle/web/ajax/ChromosomeBrowserImpl.java
+++ src/gov/nih/nci/eagle/web/ajax/ChromosomeBrowserImpl.java
@@ -2,7 +2,13 @@ package gov.nih.nci.eagle.web.ajax;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.ListIterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class ChromosomeBrowserImpl implements ChromosomeBrowser {
 
@@ -17,6 +23,56 @@ public class ChromosomeBrowserImpl implements ChromosomeBrowser {
         return expressions;
     }
 
+    public String getDataForRegionJSON(String chromosome, Long start, Long end)	{
+    	List expressions = getDataForRegion(chromosome, start, end);
+    	
+    	JSONArray repArray = new JSONArray();
+    	ListIterator listItr = expressions.listIterator();
+    	
+    	LinkedHashMap reportersMap = new LinkedHashMap(); //map of JSONObjects with repId as Key
+    	
+    	JSONObject rep = null;
+    	JSONArray dataTypes = null;
+    	
+    	while(listItr.hasNext()) {
+    		ExpressionValue ev = (ExpressionValue) listItr.next();
+    		//loop thru reporters	
+    		//is this entry already in the Map? composite key: rep|start|end
+    		if(reportersMap.containsKey(ev.getReporterId() + "|" + ev.getStart() + "|" + ev.getEnd()))	{
+    			//already exists
+    			rep = (JSONObject) reportersMap.get(ev.getReporterId() + "|" + ev.getStart() + "|" + ev.getEnd());
+    			dataTypes = (JSONArray) rep.get("dataTypes");
+    		}
+    		else	{
+    			rep = new JSONObject();
+		    	rep.put("reporterId", ev.getReporterId());
+		    	rep.put("start", ev.getStart());
+		    	rep.put("end", ev.getEnd());
+		    	rep.put("chromosome", ev.getChromosome());
+		    	dataTypes = new JSONArray();
+    		}
+   		 
+    		//loop through datatypes for this reporter
+    		JSONObject singleData = new JSONObject();
+    		singleData.put("dataType", ev.getSampleType().replace(" ", "").replace("-", ""));
+    		singleData.put("foldChange", ev.getFoldChange());
+    		dataTypes.add(singleData);
+    		
+    		//put the new or updated JSONArray back in
+	    	rep.put("dataTypes", dataTypes);
+    	
+	    	reportersMap.put(ev.getReporterId() + "|" + ev.getStart() + "|" + ev.getEnd(), rep);
+	    	//repArray.add(rep);
+    	}
+    	
+    	//convert the LinkedHashMap to JSONArray
+		for (Iterator it = reportersMap.values().iterator(); it.hasNext();) {
+		      repArray.add((JSONObject) it.next());
+		}
+		
+		return repArray.toString();
+    }
+    
     public Collection<ChromosomeCytoband> getChromosomeCytobands(String chr) {
 
         Collection<ChromosomeCytoband> cytobands = annotationService
